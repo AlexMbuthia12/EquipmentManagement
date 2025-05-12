f63ebe581ff217276df182002fc7b67aa1ef9ce9
CleopasMMuchiri <cleopasmmuchiri@gmail.com>
2025-05-12 13:21:50 +0300

    notfound page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./pages/user/Dashboard/index";
-
+import NotFound from "../NotFound.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,6 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Dashboard />} />
+          <Route path="/*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
