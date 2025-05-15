d1d8eda2d07417a73d3901f210aba882a5ae76aa
CleopasMMuchiri <cleopasmmuchiri@gmail.com>
2025-05-15 10:48:02 +0300

    login page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/user/Dashboard/index";
+import Landing from "./pages/landingPage/LandingPage";
 import MyBookings from "./pages/user/Orders/MyBookings";
+import { Toaster } from "react-hot-toast";
 // import HistoryPage from "./components/HistoryPage";
 // import ProfilePage from "./components/ProfilePage";
 // import SettingsPage from "./components/SettingsPage";
@@ -8,15 +10,20 @@ import MyBookings from "./pages/user/Orders/MyBookings";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="account/mybookings" element={<MyBookings />} />
-        {/* <Route path="/history" element={<HistoryPage />} />
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="account/mybookings" element={<MyBookings />} />
+          {/* <Route path="/history" element={<HistoryPage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/borrow/:id" element={<BorrowPage />} /> */}
-      </Routes>
-    </Router>
+        </Routes>
+      </Router>
+
+      <Toaster position="top-right" />
+    </>
   );
 }
